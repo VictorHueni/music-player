c47ac84c72c44fea58cb95ba0581f99c46377b43
Victor Hüni <victor.hueni@protonmail.com>
2021-06-13 18:52:04 +0200

    coding the progress bar functionnality : update progress bar, update current time and duration, auto next at the end...

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist'); 
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -77,6 +81,51 @@ function switchSong(isPrevious) {
 // On Load select first song
 loadSong(songs[songIndex]);
 
+// Update progress bar and time
+function updateProgressBar(e) {
+    if(isPlaying){
+        const { duration, currentTime } = e.srcElement;
+
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+
+        //Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if( durationSeconds < 10 ) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        
+        // Delay switching duration to avoid NaN:NaN
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+
+        //Calculate display for current time
+        let currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if( currentSeconds < 10 ) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+
+    }
+}
+
+// Set progress bar
+function setProgressBar(e) {
+    const width = this.clientWidth;
+    const offsetX = e.offsetX;
+    const { duration } = music;
+    const positionSeconds = (offsetX / width) * duration;
+    music.currentTime = positionSeconds;
+}
+
 // Prev or Next Event Listener
 prevBtn.addEventListener('click', () => switchSong(true));
 nextBtn.addEventListener('click', () => switchSong(false));
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener('ended', () => switchSong(false));
---
+++ to-do.md
@@ -0,0 +1,4 @@
+- [ ] Explore Spotify APi
+- [ ] Add Volume controls
+- [ ] Add the loop function
+- [ ] add the shuffle
