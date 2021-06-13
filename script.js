c774549535a98c0fae24b5d2866e01ff85211a2c
Victor Hüni <victor.hueni@protonmail.com>
2021-06-13 18:10:06 +0200

    music player next/prev with js

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist'); 
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
-const mextBtn = document.getElementById('next');
+const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+        name: "jacinto-1",
+        displayName: "Electric Chill Machine",
+        artist: "Jacinto Design"
+    },
+    {
+        name: "jacinto-2",
+        displayName: "Seven Nation Army (Remix)",
+        artist: "Jacinto Design"
+    },
+    {
+        name: "jacinto-3",
+        displayName: "Goodnight, Disco Queen",
+        artist: "Jacinto Design"
+    },
+    {
+        name: "metric-1",
+        displayName: "Front Row (Remix)",
+        artist: "Metric/Jacinto Design"
+    }
+];
 
 // Check if playing
 let isPlaying = false;
@@ -24,3 +51,32 @@ function pauseSong() {
 
 // Play or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+// Current song
+let songIndex = 0;
+
+// Prev Song
+function switchSong(isPrevious) {
+    if(isPrevious) {
+        songIndex > 0 ? songIndex-- : songIndex = songs.length - 1;
+    } else {
+        songIndex < songs.length - 1 ? songIndex++ : songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// On Load select first song
+loadSong(songs[songIndex]);
+
+// Prev or Next Event Listener
+prevBtn.addEventListener('click', () => switchSong(true));
+nextBtn.addEventListener('click', () => switchSong(false));
